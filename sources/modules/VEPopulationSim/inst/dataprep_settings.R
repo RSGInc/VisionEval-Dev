f542d124c5e55e9b583e4d0fc1cba7f251ecbdd4
Nick Fournier <nick.fournier@rsginc.com>
2022-07-06 15:35:28 -0700

    successful populationsim run, next need to feed output into VE

--- sources/modules/VEPopulationSim/inst/dataprep_settings.R
+++ sources/modules/VEPopulationSim/inst/dataprep_settings.R
@@ -1,8 +1,28 @@
 
 params = list(
-  geography = "cb_2018_41_bg_500k.zip",
-  zone_list = "bg_list.csv",
+  geography = "tl_2020_41_bg.zip",
+  zone_list = "bzone_lat_lon.csv",
   pumaxwalk = "2010_Census_Tract_to_2010_PUMA.txt",
   popsim_dir = 'populationsim_example'
 )
 
+
+
+# PopulatimSim to VE Crosswalk
+# Age0to14
+# Age15to19
+# Age20to29
+# Age30to54
+# Age55to64
+# Age65Plus
+# Wkr15to19
+# Wkr20to29
+# Wkr30to54
+# Wkr55to64
+# Wkr65Plus
+# AvePerCapInc
+# HouseType
+# Income
+# HhSize
+# HhWeight
+# HhType
