d6420fe0a2cd0f57059ca8a6e83aee950c5fb4db
Jeremy Raw <jeremy@jeremyraw.com>
2022-12-13 16:31:08 -0500

    Incremental advance on tests for Dynamic and Snapshot

--- sources/framework/visioneval/R/module.R
+++ sources/framework/visioneval/R/module.R
@@ -1517,6 +1517,7 @@ processModuleSpecs <- function(Spec_ls) {
   if (!is.null(Spec_ls$RunBy)) {
     Out_ls$RunBy <- Spec_ls$RunBy
   } else {
+    writeLog("Forcing RunBy into specification list",Level="info")
     Out_ls$RunBy <- "Region"
   }
   if (!is.null(Spec_ls$NewInpTable)) {
